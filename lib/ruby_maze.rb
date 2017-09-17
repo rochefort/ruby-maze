e1739730eb92886c2bfa4bae9ef4c28c0eaf6929
rochefort <terasawan@gmail.com>
2017-09-17 16:39:19 +0900

    Remove executor.rb

--- lib/ruby_maze.rb
+++ lib/ruby_maze.rb
@@ -1,14 +1,33 @@
-require "ruby_maze/executor"
 require "ruby_maze/keyboard"
 require "ruby_maze/maze"
 require "ruby_maze/version"
 
 class RubyMaze
-  def run(size)
-    executor = Executor.new(size)
+  def initialize(size)
+    @maze = RubyMaze::Maze.new(size)
+  end
+
+  def run
     while true
       key = Keyboard.read_key
-      executor.exec(key)
+      exec(key)
     end
   end
+
+  private
+    def exec(key)
+      case key
+      when "q", "\u0003"
+        # q or control-c
+        fail()
+      when "R"
+        @maze.reset()
+      when "N"
+        @maze.renew()
+      when "H"
+        @maze.help()
+      else
+        @maze.execute(key)
+      end
+    end
 end
